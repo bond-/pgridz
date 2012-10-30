0842ee252652190b9e24a5021bd56dd37285e6f7
Raviteja Lokineni <raviteja.lokineni@gmail.com>
2012-10-31 01:42:05 +0530

    Enabling client validation on register form

--- protected/views/user/_form.php
+++ protected/views/user/_form.php
@@ -10,6 +10,9 @@
 	'id'=>'user-form',
 	'enableAjaxValidation'=>true,
 	'enableClientValidation'=>true,
+    'clientOptions'=>array(
+        'validateOnSubmit'=>true,
+    ),
 )); ?>
 
 	<p class="note">Fields with <span class="required">*</span> are required.</p>
