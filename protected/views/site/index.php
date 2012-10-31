7e489097013cbadf0a11a6506e7ec0bc7080ea40
bond- <raviteja.lokineni@gmail.com>
2012-10-31 16:08:21 +0530

    Adding some default content

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,17 +4,10 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-
-<p>Congratulations! You have successfully created your Yii application.</p>
-
-<p>You may change the content of this page by modifying the following two files:</p>
+<h1>Network like a Professional. For FREE!</h1>
 <ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+    <li>Keep track of all your important contacts and access them from anywhere.</li>
+    <li>Add detailed notes, tags and sort by contacts and companies.</li>
+    <li>Print your company notes to help remember details during BankWeeks, on-campus visit sessions etc.</li>
+    <li>Many more features coming soon!<a href="suggest.php"> Suggest new features to better serve your needs.</a></li>
 </ul>
-
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
