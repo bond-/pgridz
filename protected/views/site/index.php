c2bedbf9f5a67bcfa01dbe78ef6cbbc65f681288
bond- <raviteja.lokineni@gmail.com>
2012-10-31 16:46:53 +0530

    Fixing link on home page

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -9,5 +9,5 @@ $this->pageTitle=Yii::app()->name;
     <li>Keep track of all your important contacts and access them from anywhere.</li>
     <li>Add detailed notes, tags and sort by contacts and companies.</li>
     <li>Print your company notes to help remember details during BankWeeks, on-campus visit sessions etc.</li>
-    <li>Many more features coming soon!<a href="suggest.php"> Suggest new features to better serve your needs.</a></li>
+    <li>Many more features coming soon! <?php echo CHtml::link('Suggest new features to better serve your needs.',array('/site/contact'));?></li>
 </ul>
