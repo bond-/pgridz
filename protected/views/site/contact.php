50fff5038772ddb56ddc6735dadadc2784e0667a
Raviteja Lokineni <raviteja.lokineni@gmail.com>
2012-11-04 17:34:52 +0530

    Fixing layout of about.php and contact.php

--- protected/views/site/contact.php
+++ protected/views/site/contact.php
@@ -1,7 +1,7 @@
 <?php
 /* @var $this SiteController */
 /* @var $model ContactForm */
-/* @var $form CActiveForm */
+/* @var $form TbActiveForm */
 
 $this->pageTitle=Yii::app()->name . ' - Suggestion/Comment';
 $this->breadcrumbs=array(
@@ -14,72 +14,37 @@ $this->breadcrumbs=array(
 <?php if(Yii::app()->user->hasFlash('contact')): ?>
 
 <div class="flash-success">
-	<?php echo Yii::app()->user->getFlash('contact'); ?>
+    <?php echo Yii::app()->user->getFlash('contact'); ?>
 </div>
 
 <?php else: ?>
 
 <p>
-If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
+    If you have business inquiries or other questions, please fill out the following form to contact us. Thank you.
 </p>
 
 <div class="form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-	'id'=>'contact-form',
-	'enableClientValidation'=>true,
-	'clientOptions'=>array(
-		'validateOnSubmit'=>true,
-	),
+    <?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm', array(
+    'id'=>'contact-form',
+    'enableClientValidation'=>true,
+    'clientOptions'=>array(
+        'validateOnSubmit'=>true,
+    ),
+    'htmlOptions'=>array('class'=>'well'),
 )); ?>
-
-	<p class="note">Fields with <span class="required">*</span> are required.</p>
-
-	<?php echo $form->errorSummary($model); ?>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'name'); ?>
-		<?php echo $form->textField($model,'name'); ?>
-		<?php echo $form->error($model,'name'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'email'); ?>
-		<?php echo $form->textField($model,'email'); ?>
-		<?php echo $form->error($model,'email'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'subject'); ?>
-		<?php echo $form->textField($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
-		<?php echo $form->error($model,'subject'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'body'); ?>
-		<?php echo $form->textArea($model,'body',array('rows'=>6, 'cols'=>50)); ?>
-		<?php echo $form->error($model,'body'); ?>
-	</div>
-
-	<?php if(CCaptcha::checkRequirements()): ?>
-	<div class="row">
-		<?php echo $form->labelEx($model,'verifyCode'); ?>
-		<div>
-		<?php $this->widget('CCaptcha'); ?>
-		<?php echo $form->textField($model,'verifyCode'); ?>
-		</div>
-		<div class="hint">Please enter the letters as they are shown in the image above.
-		<br/>Letters are not case-sensitive.</div>
-		<?php echo $form->error($model,'verifyCode'); ?>
-	</div>
-	<?php endif; ?>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Submit'); ?>
-	</div>
-
-<?php $this->endWidget(); ?>
-
+    <p class="note">Fields with <span class="required">*</span> are required.</p>
+    <?php echo $form->textFieldRow($model,'name'); ?>
+    <?php echo $form->textFieldRow($model,'email'); ?>
+    <?php echo $form->textFieldRow($model,'subject',array('size'=>60,'maxlength'=>128)); ?>
+    <?php echo $form->textAreaRow($model,'body',array('rows'=>6, 'cols'=>50)); ?>
+    <?php if(CCaptcha::checkRequirements()): ?>
+    <div>
+        <?php echo $form->captchaRow($model,'verifyCode',array('hint'=>'Please enter the letters as they are shown in the image above.<br/>Letters are not case-sensitive.')); ?>
+    </div>
+    <?php endif; ?>
+    <?php $this->widget('bootstrap.widgets.TbButton', array('buttonType'=>'submit', 'label'=>'Submit', 'type'=>'primary', 'size'=>'normal')); ?>
+    <?php $this->endWidget(); ?>
 </div><!-- form -->
 
 <?php endif; ?>
