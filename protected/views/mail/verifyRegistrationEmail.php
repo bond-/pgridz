a7f9190bd46aa95d6fc9624768587432b1748496
bond- <raviteja.lokineni@gmail.com>
2012-11-26 11:59:53 +0530

    Hyperlinks in email

--- protected/views/mail/verifyRegistrationEmail.php
+++ protected/views/mail/verifyRegistrationEmail.php
@@ -1,18 +1,8 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: mj
- * Date: 10/11/12
- * Time: 5:01 PM
- * To change this template use File | Settings | File Templates.
- */
-
-?>
 Hi,
 <br/><br/>
 You (or someone pretending to be you) created an account with this email address.
 <br/><br/>
-If you made the request, please click the following link to finish the registration:<?php echo $link?>
+If you made the request, please click the following link to finish the registration: <a href="<?php echo $link?>" target="_blank"><?php echo $link?></a>
 <br/><br/>
 To login please visit: <a href="<?php echo Yii::app()->createAbsoluteUrl("");?>"><?php echo CHtml::encode(Yii::app()->name)?></a>
 <br/><br/><br/>
